3bf664358a69fa4b9ec604a7a0a06b0115e7716c
GrebenyukV <35668128+GrebenyukV@users.noreply.github.com>
2018-10-05 16:36:17 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// telegram token
+var token = 'XXXXXXXXXXXXXXXXXXXXXX'
+// firebase config
+var config = {
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXX.firebaseapp.com",
+    databaseURL: "https://XXXXXXXXX.firebaseio.com/",
+    projectId: "XXXXXXXX",
+    storageBucket: "XXXXXXXXXXXX.appspot.com",
+};
+
+exports.token = token;
+exports.firebase_config = config;
